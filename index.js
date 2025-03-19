c5e02cbab59f4a14aafdbbd378cf4816966cf14c
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-03-19 20:51:50 +0300

    update  max function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 // Write your code here
 
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
 
 
 let num1 = 31;
@@ -11,15 +12,14 @@ const multiply = num1 * num2;
 console.log(multiply);
 
 
-
+// Create a variable called random that will generate a random integer greater than 0.
 
 const random = Math.floor(Math.random() * 100) + 1;
 
 console.log(random);
 
+
 // Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
-
-
 
 let num3 = 10;
 let num4 = 6;
@@ -38,11 +38,13 @@ console.log(mod(num3, num4));
 // Create a variable called max that finds the highest number in a set; the value returned should be 20.
 
 
+// const numSet = [5,8,12,20,7];
+// const max = Math.max(...new numSet([5,8,12,20,7]));
 
-const numSet = [3,5,7,9,13,20,17];
-const max = Math.max(...numSet);
+// const numSet = [5, 12, 20, 8, 15];
+// const max = Math.max(...new numSet([5, 12, 20, 8, 15]));
 
-let results = Math.max(...numSet);
+// console.log(max);
 
-console.log(max);
+// const max = Math.max(5, 12, 20, 8, 15);
 
